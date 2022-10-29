a029111dab8d8c6727f3bdafc306d81d10aa9e30
Sandip <35768586+bigsandip@users.noreply.github.com>
2022-10-28 22:21:12 -0400

    Update db.js
    udated database

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,7 @@
 module.exports = {
-  //local MongoDB deployment ->
-  URI: "mongodb://127.0.0.1/car_store",
+  
+  // URI: "mongodb://127.0.0.1/car_store",
+
+  URI:"mongodb+srv://bigsandip:0JQ1hWJt2CJ7D9JC@carcluster.dltvyim.mongodb.net/?retryWrites=true&w=majority",
+  
 };
